6e8237f16068cb452965a89996492f7cc0b97be8
Joseph Hughes <Joseph.Hughes@version1.com>
2019-01-31 15:06:56 +0000

    add my okta account details

--- custom-login/util/default-config.ts
+++ custom-login/util/default-config.ts
@@ -1,7 +1,7 @@
 export default {
   oidc: {
-    clientId: '{clientId}',
-    issuer: 'https://{yourOktaDomain}.com/oauth2/default',
+    clientId: '0oaj4i4i02q7mp2zF0h7',
+    issuer: 'https://dev-901583.oktapreview.com/oauth2/default',
     redirectUri: 'http://localhost:8080/implicit/callback',
     scope: 'openid profile email'
   },
